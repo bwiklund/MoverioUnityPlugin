ad362fd281d0936eef47be02e6858a25713c7d5b
Ben Wiklund <ben@daisyowl.com>
2015-04-22 15:56:43 -0700

    breaking everything

--- Unity3D/Assets/MoverioController/Scripts/HeadTrackingAR.cs
+++ Unity3D/Assets/MoverioController/Scripts/HeadTrackingAR.cs
@@ -3,57 +3,39 @@ using System.Collections;
 
 [AddComponentMenu("Moverio/HeadTrackingAR")]
 
-public class HeadTrackingAR : MonoBehaviour
-{
+public class HeadTrackingAR : MonoBehaviour {
 
-	private static HeadTrackingAR _instance;
-	public static HeadTrackingAR Instance
-	{
-		get
-		{
-			return _instance;
-		}
-	}
+    private static HeadTrackingAR _instance;
+    public static HeadTrackingAR Instance {
+        get {
+            return _instance;
+        }
+    }
 
-	bool _gyroIsAvailable;
-	Gyroscope _head;
-	Quaternion _callibration;
-	Transform _t;
-	
-	float yO = 0.0f;
+    bool gyroIsAvailable;
 
-	void Awake()
-	{
-		_instance = this;
-	}
+    void Awake() {
+        _instance = this;
+    }
 
-	
-	void Start()
-	{
-		_t = transform;
+    void Start() {
+        gyroIsAvailable = SystemInfo.supportsGyroscope;
 
-		yO = _t.position.y;
+        if (SystemInfo.supportsGyroscope) {
+            Gyroscope headGyro = Input.gyro;
+            Input.gyro.enabled = true;
+            Input.gyro.updateInterval = 1f / 60f;
+            headGyro.updateInterval = 1f / 60f;
+        }
+    }
 
-		_gyroIsAvailable = SystemInfo.supportsGyroscope;
-		
-		if (_gyroIsAvailable) 
-		{		
-			_head = Input.gyro;
-			Input.gyro.enabled = true;
-			Input.gyro.updateInterval = 1f/60f;
-			_head.updateInterval = 1f/60f;
-		}
-	}
+    void LateUpdate() {
+        // TODO: there a some way to reduce this but quaternion multiplication is not commutative and am in a rush.
+        Quaternion baseRotation = Quaternion.Euler(new Vector3(90, 180, 0));
+        Quaternion callibration = Quaternion.Euler(new Vector3(0, 0, 180));
 
-	
-	void Update()
-	{
-		transform.eulerAngles = new Vector3(90,180,0);
-		_callibration = Quaternion.Euler(new Vector3(0,0,180));
-		
-		if (_gyroIsAvailable) 
-		{
-			_t.localRotation = Input.gyro.attitude * _callibration;//Quaternion.Slerp(transform.localRotation, Input.gyro.attitude * _callibration, Time.deltaTime*30);
-		}
-	}
+        if (gyroIsAvailable) {
+            transform.localRotation = baseRotation * Input.gyro.attitude * callibration;//Quaternion.Slerp(transform.localRotation, Input.gyro.attitude * _callibration, Time.deltaTime*30);
+        }
+    }
 }
